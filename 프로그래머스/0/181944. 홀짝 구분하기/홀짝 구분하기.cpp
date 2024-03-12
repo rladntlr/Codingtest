ab032f199c1aec6633dc3d86cf36ffe86e66c1fb
rladntlr <127586693+rladntlr@users.noreply.github.com>
2024-03-12 23:00:47 +0900

    [level 0] Title: 홀짝 구분하기, Time: 2.65 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n;
+    cin >> n;
+    if(n%2==0) cout << n << " is even";
+    else cout << n << " is odd";
+    return 0;
+}

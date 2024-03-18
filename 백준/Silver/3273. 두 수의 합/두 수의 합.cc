aa03e5a5108acbe7fdab878cb36e59d1ccca49df
rladntlr <127586693+rladntlr@users.noreply.github.com>
2024-03-19 00:38:12 +0900

    [Silver III] Title: 두 수의 합, Time: 12 ms, Memory: 10228 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n, x, ans = 0;
+    vector<int> num(100005, 0);
+    vector<int> check(2000005, 0);
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> num[i];
+    }
+    cin >> x;
+    for (int i = 0; i < n; i++) {
+        if (x - num[i] > 0 && check[x - num[i]]) ans++;
+        check[num[i]]++;
+    }
+    cout << ans;
+}

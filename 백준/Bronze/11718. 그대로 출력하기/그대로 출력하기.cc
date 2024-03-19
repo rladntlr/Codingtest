f06028db6c77411ddc39722df2ef5729b0ab5b3d
rladntlr <127586693+rladntlr@users.noreply.github.com>
2024-03-19 22:58:18 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string s;
+    while(1) {
+        getline(cin, s);
+        if(s=="") break;
+        cout << s << '\n';
+    }
+}

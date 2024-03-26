afe4f140c1648b4b66a9f55787e5610710d40d0f
rladntlr <127586693+rladntlr@users.noreply.github.com>
2024-03-26 15:59:10 +0900

    [level 0] Title: 코드 처리하기, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string solution(string code) {
+    int mode = 0;
+    string ret;
+    for(int i = 0;i<code.length();i++){
+        if(mode == 0){
+            if(code[i]!='1'){
+                if(i%2==0) ret += code[i];
+            }
+            else mode = 1;
+        }
+        else{
+            if(code[i]!='1'){
+                if(i%2==1) ret += code[i];
+            }
+            else mode=0;
+        }
+    }
+    if(ret.empty()){
+        return "EMPTY";
+    }
+    else return ret;
+}

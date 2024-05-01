a4cb15f64f5ecffdd9266c8554bc9bb1d4748907
rladntlr <127586693+rladntlr@users.noreply.github.com>
2024-05-01 22:07:57 +0900

    [level 0] Title: 카운트 업, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(int start_num, int end_num) {
+    vector<int> answer;
+    for(int i = start_num;i<=end_num;i++)
+        answer.push_back(i);
+    return answer;
+}

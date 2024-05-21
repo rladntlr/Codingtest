8f681935469b5390a4157232f4867502db1a7b29
rladntlr <127586693+rladntlr@users.noreply.github.com>
2024-05-21 22:07:33 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(string my_string, vector<int> index_list) {
+    string answer = "";
+    
+    for(int i = 0; i < index_list.size(); ++i) answer += my_string[index_list[i]];
+    return answer;
+}

96b514d9c217a80ba6c0f84eb4fc612bcbc9d1c2
rladntlr <127586693+rladntlr@users.noreply.github.com>
2024-07-16 15:54:44 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.01 ms, Memory: 3.62 MB -BaekjoonHub

---
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list, int n) {
+    vector<int> answer;
+    for(int i = n-1; i < num_list.size(); i++) answer.push_back(num_list[i]);
+    return answer;
+}

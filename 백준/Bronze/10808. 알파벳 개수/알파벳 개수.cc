68a40110946dddf19424fcbcb43af3977cc51043
rladntlr <127586693+rladntlr@users.noreply.github.com>
2024-07-18 16:13:50 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
     string s;
     cin >> s;
-    vector<int> word(26,0);
-    for(auto num:s){
-        word[num-'a']++;
-    }
-    for(int i=0;i<word.size();i++)
-        cout << word[i] << ' ';
+    vector<int> v(26);
+
+    for(auto c : s) v[c-'a']++;
+    for(auto c : v) cout << c << ' ';
 }

10afc3daea467f6b9e9ea5c290bd84d4132f5b19
rladntlr <127586693+rladntlr@users.noreply.github.com>
2024-07-19 14:09:33 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.01 ms, Memory: 4.01 MB -BaekjoonHub

---
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(string myString) {
+    transform(myString.begin(), myString.end(), myString.begin(), ::toupper);
+    return myString;
+}

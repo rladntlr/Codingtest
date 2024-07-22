584a6f68fc5d10e79eba0ae3bce5e52d901000cf
rladntlr <127586693+rladntlr@users.noreply.github.com>
2024-07-22 13:58:35 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.04 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(string myString, string pat) {
+    int answer = 0;
+    for(int i = 0; i <= myString.length() - pat.length(); i++){
+        if(myString.substr(i,pat.length())==pat) answer++;
+    }
+    return answer;
+}

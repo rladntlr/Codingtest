93192c11ebcfa5799be53f45b542b1a5c8ef8f41
rladntlr <127586693+rladntlr@users.noreply.github.com>
2024-07-24 14:29:51 +0900

    [level 0] Title: 세 개의 구분자, Time: 68.23 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> solution(string myStr) {
+    vector<string> answer;
+    string s;
+    for(int i = 0; i < myStr.length(); i++){
+        if(myStr[i]!='a' && myStr[i]!='b' && myStr[i]!='c') s +=myStr[i];
+        else if(s!=""){
+            answer.push_back(s);
+            s = "";
+        }
+    }
+    if(s!="") answer.push_back(s);
+    else answer.push_back("EMPTY");
+    return answer;
+}

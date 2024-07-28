325eef0cdfc8a35898805cc03c994f4ccaca79ec
rladntlr <127586693+rladntlr@users.noreply.github.com>
2024-07-28 21:19:20 +0900

    [Gold V] Title: 탑, Time: 100 ms, Memory: 6300 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -1,23 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define X first
-#define Y second
 
-int N;
-stack<pair<int,int>> tower;
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-int main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
+    int n;
+    cin >> n;
+    stack<pair<int, int>> s;
+    s.push({100000001,0});
 
-  cin >> N;  
-  tower.push({100000001, 0});
-  for (int i = 1; i <= N; i++) {
-    int height;
-    cin >> height;
-    while (tower.top().X < height)
-      tower.pop();    
-    cout << tower.top().Y << " ";
-    tower.push({height, i});      
-  }
+    for(int i = 1; i <= n; i++){
+        int height;
+        cin >> height;
+        while(s.top().first < height){
+            s.pop();
+        }
+        cout << s.top().second << " ";
+        s.push({height,i});
+    }
 }

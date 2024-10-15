bfc5730dc6898c94687bf2cb88819ddc5be06b16
rladntlr <127586693+rladntlr@users.noreply.github.com>
2024-10-16 02:08:55 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.02 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    sort(num_list.begin(), num_list.end());
+    vector<int> answer(num_list.begin(), num_list.begin()+5);
+    return answer;
+}

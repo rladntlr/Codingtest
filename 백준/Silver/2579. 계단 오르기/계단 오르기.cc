40c9dfbe2260c26ff7b014751c427c9629cc1b8c
rladntlr <127586693+rladntlr@users.noreply.github.com>
2024-11-13 20:00:48 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int d[302][3];
+int s[302];
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    for(int i = 1; i<=n; i++) cin >> s[i];
+    if(n==1){
+        cout << s[1];
+        return 0;
+    }
+    d[1][1] = s[1];
+    d[1][2] = 0;
+    d[2][1] = s[2];
+    d[2][2] = s[1] + s[2];
+    for(int i = 3; i<=n; i++){
+        d[i][1] = max(d[i-2][1],d[i-2][2])+s[i];
+        d[i][2] = d[i-1][1] + s[i];
+    }
+    cout << max(d[n][1],d[n][2]);
+}

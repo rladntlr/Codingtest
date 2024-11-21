d651f86bfa8322b013f6c0d2a4f58ba6acbf104d
rladntlr <127586693+rladntlr@users.noreply.github.com>
2024-11-21 23:51:23 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer;
+    reverse(num_list.begin(), num_list.end());
+    return num_list;
+}

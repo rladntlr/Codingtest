5f07a9f1afae860a173221b6bb85fdde7839cde8
rladntlr <127586693+rladntlr@users.noreply.github.com>
2024-11-25 22:24:13 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(string my_string, string letter) {
+    string answer = "";
+    
+    for(int i=0; i<my_string.size(); i++)
+        if(my_string[i]!=letter[0]) answer+=my_string[i];
+    
+    return answer;
+}

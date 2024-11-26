f51089471d58f404d01e8eb29f1d02b36780ee1d
rladntlr <127586693+rladntlr@users.noreply.github.com>
2024-11-26 18:14:18 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(int angle) {
+    if(angle == 180) return 4;
+    else if(angle <180 && angle >90) return 3;
+    else if(angle==90) return 2;
+    else if(angle <90 && angle >0) return 1;
+}

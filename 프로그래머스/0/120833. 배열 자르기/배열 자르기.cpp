a1ae91927b26560c460f415c6facfc230bf75ad5
rladntlr <127586693+rladntlr@users.noreply.github.com>
2024-11-26 23:06:24 +0900

    [level 0] Title: 배열 자르기, Time: 0.01 ms, Memory: 4.43 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers, int num1, int num2) {
+    vector<int> answer;
+    for(int i  = num1; i <= num2; i++){
+        answer.push_back(numbers[i]);
+    }
+    return answer;
+}

3c72b30158d7c36485b85850eeaaecaae09ec3fa
rladntlr <127586693+rladntlr@users.noreply.github.com>
2024-11-27 19:21:07 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+
+int solution(int hp) {
+    int answer = 0;
+    answer += hp / 5;
+    hp %= 5;
+    answer += hp / 3;
+    hp %= 3;
+    answer += hp / 1;
+    hp %= 1;
+    return answer;
+}

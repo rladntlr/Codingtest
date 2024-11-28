613f63495bc739b05534cce35762e83abd477975
rladntlr <127586693+rladntlr@users.noreply.github.com>
2024-11-29 00:45:28 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.cc
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -1,8 +1,10 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int a, b;
-    cin >> a >> b;
-    cout << a+b;
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n, x;
+    cin >> n >> x;
+    cout << n + x;
 }

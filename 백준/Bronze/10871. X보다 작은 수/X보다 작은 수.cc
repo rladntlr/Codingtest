70a99136ae33ac09d33d0481bdb83000311aef54
rladntlr <127586693+rladntlr@users.noreply.github.com>
2024-11-29 00:44:03 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -1,14 +1,15 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int n, x, t;
-    cin>>n>>x;
-    while (n--){
-        cin>>t;
-        if(t<x)
-            cout<<t<<' ';
+    int n, x;
+    cin >> n >> x;
+    for(int i = 0; i < n; i++){
+        int k;
+        cin >> k;
+        if(k < x)
+            cout << k << '\n';
     }
 }

eb6357b7c5d2f74e7e5a0e6c55aaf9f9c3375861
rladntlr <127586693+rladntlr@users.noreply.github.com>
2024-11-29 00:46:07 +0900

    [Bronze V] Title: Hello World, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.cc
+++ 백준/Bronze/2557. Hello World/Hello World.cc
@@ -1,7 +1,8 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     cout << "Hello World!";
-    return 0;
 }

28470a7e424edd1dfe792ab56ac24327e07d869d
rladntlr <127586693+rladntlr@users.noreply.github.com>
2024-12-10 01:31:32 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.01 ms, Memory: 3.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    int a = sqrt(n);
+    if( a == sqrt(n)) return 1;
+    else return 2;
+}

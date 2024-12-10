855a051ad413a115ea197651bf9dbbcda4d8ad62
rladntlr <127586693+rladntlr@users.noreply.github.com>
2024-12-10 20:05:15 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> array, int height) {
+    // 람다 표현식에서 height를 캡처하여 비교
+    int under_height = count_if(array.begin(), array.end(),
+                                [height](int answer) { return answer > height; });
+    
+    return under_height;
+}

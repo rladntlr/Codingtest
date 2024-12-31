52d7853c733e3f5601917220eff50e3c5ddd87c9
rladntlr <127586693+rladntlr@users.noreply.github.com>
2024-12-31 15:32:32 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.cc
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -1,13 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a[3];
-
-int main() {
+int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
-    for(int i=0; i<3; i++) cin >> a[i];
-    sort(a,a+3);
-    for(int i=0; i<3; i++) cout << a[i] << ' ';
+    int arr[3];
+    int a, b, c;
+    for(int i = 0; i < 3; i++) cin >> arr[i];
+    sort(arr, arr+3);
+    for(auto i : arr)
+        cout << i << " ";
 }

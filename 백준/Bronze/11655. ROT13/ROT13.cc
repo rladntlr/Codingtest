62de42cfdeed5fcaa1fb81db905c18e5142f602a
rladntlr <127586693+rladntlr@users.noreply.github.com>
2025-06-09 16:51:04 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.cc
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -1,22 +1,20 @@
+//
+// Created by 김우식 on 25. 6. 9.
+//
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int cnt[26];
 int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(0);
-
     string s;
-
     getline(cin, s);
-
     for(int i = 0; i < s.size(); i++){
-        if(s[i] >= 65 && s[i] < 91){ // 대문자
-            if(s[i] + 13 > 90) s[i] = s[i] - 13;
+        if(s[i]>=65 && s[i]<=90 ){
+            if(s[i]+13>90) s[i] = s[i] + 13 - 26;
             else s[i] = s[i] + 13;
         }
-        else if(s[i] >= 97 && s[i] <= 122){ // 소문자
-            if(s[i] + 13 > 122) s[i] = s[i] - 13;
+        else if(s[i] >= 97 && s[i] <=122 ){
+            if(s[i] + 13 > 122) s[i] = s[i] +13 - 26;
             else s[i] = s[i] + 13;
         }
         cout << s[i];

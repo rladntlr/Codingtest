a2a620ab5e5c5aa6681110659621e18dd4c0ea3f
rladntlr <127586693+rladntlr@users.noreply.github.com>
2025-06-17 17:34:48 +0900

    [Silver IV] Title: 주몽, Time: 0 ms, Memory: 4032 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.cc
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -1,21 +1,43 @@
+////
+//// Created by 김우식 on 25. 6. 17.
+////
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//int a[15002];
+//int main(){
+//    int n, m, cnt = 0;
+//    cin >> n >> m;
+//    for(int i = 0; i < n; i++) cin >> a[i];
+//    if(m > 20000) cout << 0 << '\n';
+//    else{
+//        for(int i = 0; i < n; i++){
+//            for(int j = i + 1; j < n; j++)
+//                if(a[i] + a[j] == m) cnt++;
+//        }
+//    }
+//    cout << cnt << '\n';
+//}
 #include <bits/stdc++.h>
 using namespace std;
 
-int cnt[15005];
-int main(){
+bool occur[2000001];
+int a[15002];
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int n, m, ans = 0;
+    int n, m, cnt = 0;
     cin >> n >> m;
-    for(int i = 0; i < n; i++) cin >> cnt[i];
-    if(m > 200000) cout << 0;
-    else{
-        for(int i = 0 ; i < n; i++){
-            for(int j = i + 1; j < n; j++){
-                if(cnt[i] + cnt[j] == m) ans++;
-            }
+    for(int i = 0; i < n; i++) cin >> a[i];
+
+    for(int i = 0; i < n; i++) {
+        // (m - a[i])가 배열에 있었으면 쌍이 만들어짐
+        // a[i] == m - a[i] 인 케이스는 두 번 등장해야 쌍이 만들어지니 이 방식으로도 자동 처리됨
+        if (m - a[i] > 0 && m - a[i] <= 2000000 && occur[m - a[i]]) {
+            cnt++;
         }
-        cout << ans;
+        occur[a[i]] = true;
     }
+    cout << cnt << '\n';
 }

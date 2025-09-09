be854da6112503ee605148c88fa7c82a2f72d54c
rladntlr <127586693+rladntlr@users.noreply.github.com>
2025-09-09 11:46:43 +0900

    [level 2] Title: 오픈채팅방, Time: 193.05 ms, Memory: 162 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,27 +1,28 @@
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.StringTokenizer;
 class Solution {
     public String[] solution(String[] record) {
-      HashMap<String, String> msg = new HashMap<>();
-        msg.put("Enter", "님이 들어왔습니다.");
-        msg.put("Leave", "님이 나갔습니다.");
+       HashMap<String, String> uid = new HashMap<>();
 
-        HashMap<String, String> uid = new HashMap<>();
-
-        for (String s : record) {
-            String[] split = s.split(" ");
-            if (split.length == 3) {
-                uid.put(split[1], split[2]);
+        for(int i = 0; i < record.length; i++){
+            char first = record[i].charAt(0);
+            if (first == 'E' || first == 'C') {
+                StringTokenizer st = new StringTokenizer(record[i]);
+                st.nextToken();
+                uid.put(st.nextToken(), st.nextToken());
             }
         }
-        ArrayList<String> ans = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
 
-        for (String s : record) {
-            String[] split = s.split(" ");
-            if(msg.containsKey(split[0])){
-                ans.add(uid.get(split[1]) + msg.get(split[0]));
+        for(int i = 0; i < record.length; i++){
+            char first = record[i].charAt(0);
+            if (first == 'E' || first == 'L') {
+                StringTokenizer st = new StringTokenizer(record[i]);
+                st.nextToken();
+                sb.append(uid.get(st.nextToken())).append( first == 'E' ? "님이 들어왔습니다.," : "님이 나갔습니다.,");
             }
         }
-        return ans.toArray(new String[0]);
+        sb.setLength(sb.length()-1);
+        return sb.toString().split(",");
     }
 }

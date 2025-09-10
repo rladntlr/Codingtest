813b24b9a927fe1e54a9ba424a874a14177decc3
rladntlr <127586693+rladntlr@users.noreply.github.com>
2025-09-10 13:21:06 +0900

    [Silver III] Title: N과 M (1), Time: 232 ms, Memory: 22660 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,11 +1,14 @@
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
     static int n, m;
-    static int[] src, tgt;
+    static int[] tgt;
     static boolean[] select;
+    static StringBuilder sb;
+
     public static void main(String[] args) throws Exception{
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,28 +17,26 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        src = new int[n + 1];
         select = new boolean[n + 1];
         tgt = new int[m];
 
-        for(int i = 1; i <= n; i++){
-            src[i] = i;
-        }
+        sb = new StringBuilder();
 
         perm(0);
+        System.out.println(sb);
     }
 
     static void perm(int tgtIdx) {
         if (tgtIdx == m ) {
             for (int n : tgt) {
-                System.out.print(n + " ");
+                sb.append(n).append(" ");
             }
-            System.out.println();
+            sb.append("\n");
             return;
         }
         for (int i = 1; i <= n; i++) {
             if (select[i] == true) continue;
-            tgt[tgtIdx] = src[i];
+            tgt[tgtIdx] = i;
             select[i] = true;
             perm(tgtIdx + 1);
             select[i] = false;

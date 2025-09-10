a388b2f1e29a350373329c8cb7a75036936c942f
rladntlr <127586693+rladntlr@users.noreply.github.com>
2025-09-10 15:45:22 +0900

    [level 2] Title: 구명보트, Time: 10.27 ms, Memory: 57.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,18 @@
+
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int answer = 0;     // 보트 수
+        int i = 0, j = people.length - 1;
+
+        while (i <= j) {
+            if (people[i] + people[j] <= limit) {
+                i++;
+            }
+            j--;
+            answer++;
+        }
+        return answer;
+    }
+}

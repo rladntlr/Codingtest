d91898337c45f6a756f44306ecd1dfcb91749da8
rladntlr <127586693+rladntlr@users.noreply.github.com>
2025-09-16 15:41:59 +0900

    [Gold III] Title: 아기 상어, Time: 112 ms, Memory: 16516 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -84,6 +84,8 @@ public class Main {
                     }
                 }
             }
+
+            if (d + 1 >= minDist) continue;
             for (int i = 0; i < 4; i++) {
                 int ny = y + dy[i];
                 int nx = x + dx[i];

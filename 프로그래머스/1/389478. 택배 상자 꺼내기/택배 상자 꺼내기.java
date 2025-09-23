aec4f78b6cafb500b6a0b0b32f4fb3061eea9c8a
rladntlr <127586693+rladntlr@users.noreply.github.com>
2025-09-23 15:27:57 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.09 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -1,14 +1,45 @@
 class Solution {
     public int solution(int n, int w, int num) {
-        int answer = 0;
-        int row = (num - 1) / w;
-        int col = (row % 2 == 0) ? (num - 1) % w : (w - 1 - (num - 1) % w);
+        // 창고를 2차원 배열로 시뮬레이션
+        int[][] warehouse = new int[101][101]; // 제한 조건 n ≤ 100이므로 넉넉히
+        int box = 1; // 현재 놓을 박스 번호
+        int h = 0;   // 현재 층 (행)
+        boolean leftToRight = true; // 방향
 
-        for (; row < (n + w - 1) / w; row++) {
-            int aboveBox = (row % 2 == 0) ? col + row * w : (w - 1 - col) + row * w;
-            if (aboveBox < n) answer++;
+        // 상자를 채우기
+        while (box <= n) {
+            if (leftToRight) { // 왼쪽 -> 오른쪽
+                for (int i = 0; i < w && box <= n; i++) {
+                    warehouse[h][i] = box++;
+                }
+            } else { // 오른쪽 -> 왼쪽
+                for (int i = w - 1; i >= 0 && box <= n; i--) {
+                    warehouse[h][i] = box++;
+                }
+            }
+            h++;
+            leftToRight = !leftToRight; // 방향 전환
         }
 
-        return answer;
+        // num의 위치 찾기
+        int targetRow = -1, targetCol = -1;
+        for (int row = 0; row < h; row++) {
+            for (int col = 0; col < w; col++) {
+                if (warehouse[row][col] == num) {
+                    targetRow = row;
+                    targetCol = col;
+                    break;
+                }
+            }
+            if (targetRow != -1) break;
+        }
+
+        // 위에 쌓인 박스 개수 세기
+        int count = 0;
+        for (int row = targetRow; row < h; row++) {
+            if (warehouse[row][targetCol] != 0) count++;
+        }
+
+        return count;
     }
 }

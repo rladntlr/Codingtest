1606aa9318769d4a3c882e947b1ca5f08d470180
rladntlr <127586693+rladntlr@users.noreply.github.com>
2025-09-29 16:13:08 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,5 +1,8 @@
 select o.ANIMAL_ID, o.NAME
 from ANIMAL_OUTS o
-left join ANIMAL_INS i on o.ANIMAL_ID = i.ANIMAL_ID
-where i.ANIMAL_ID is null
+where not exists(
+    select 1
+    from ANIMAL_INS i
+    where i.ANIMAL_ID = o.ANIMAL_ID
+)
 order by o.ANIMAL_ID;

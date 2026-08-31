114d9c7eb609133560fe277301db1076607dfa2e
rladntlr <127586693+rladntlr@users.noreply.github.com>
2026-08-31 14:05:48 +0900

    [Passed] 길이 단위 변환하기 (98ms, 9MB)

---
+++ codetree/trail1/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        double feet = 30.48;
+        double mile = 160934;
+        double a = 9.2;
+        double b = 1.3;
+
+        System.out.printf("%.1fft = %.1fcm\n", a, a*feet);
+        System.out.printf("%.1fmi = %.1fcm\n", b, b*mile);
+    }
+}

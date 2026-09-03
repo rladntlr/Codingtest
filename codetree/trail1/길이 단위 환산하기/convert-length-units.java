cf45a4665af398a9b1abb74970c1dfbdc57f7c5f
rladntlr <127586693+rladntlr@users.noreply.github.com>
2026-09-03 17:32:30 +0900

    [Passed] 길이 단위 환산하기 (153ms, 12MB)

---
+++ codetree/trail1/길이 단위 환산하기/convert-length-units.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+
+        System.out.printf("%.1f", a * 30.48);
+    }
+}
